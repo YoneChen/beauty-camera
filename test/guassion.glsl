5b13838e1cb14008fdceedd4fa61e59ae489bef8
yonechen <yonechen@tencent.com>
2021-01-27 19:32:05 +0800

    feat: add shader

--- test/guassion.glsl
+++ test/guassion.glsl
@@ -7,24 +7,49 @@ uniform vec2 u_mouse;
 uniform float u_time;
 uniform sampler2D u_texture_0;
 
-// #define uv gl_FragCoord.xy/u_resolution.xy
-#define coreSize 3
-#define halfSize coreSize/2
-void main() {
-    float kernel[9];
-    kernel[6] = 1.; kernel[7] = 2.; kernel[8] = 1.;
-    kernel[3] = 2.; kernel[4] = 4.; kernel[5] = 2.;
-    kernel[0] = 1.; kernel[1] = 2.; kernel[2] = 1.;
+float normpdf(in float x, in float sigma)
+{
+	return 0.39894*exp(-0.5*x*x/(sigma*sigma))/sigma;
+}
 
-    vec2 uv = gl_FragCoord.xy/u_resolution.xy;
-    float texelOffset = 1. / 500.0;
-    vec3 color = vec3(0.);
-    for(int i = 0; i < coreSize; i++) {
-      for(int j = 0; j < coreSize; j++) {
-        vec3 currentColor = texture2D(u_texture_0,uv+vec2(float(-halfSize+i)*texelOffset,float(-halfSize+j)*texelOffset)).xyz;
-        color += currentColor * kernel[i * coreSize + j];
-      }
-    }
-    color /= 16.;
+vec3 guassionBlur(in sampler2D tex, in vec2 uv, in vec2 offset)
+{
+
+		//declare stuff
+		const int mSize = 15;
+		const int kSize = (mSize-1)/2;
+		float kernel[mSize];
+		vec3 final_colour = vec3(0.0);
+
+		//create the 1-D kernel
+		float sigma = 7.0;
+		float Z = 0.0;
+		for (int j = 0; j <= kSize; ++j)
+		{
+			kernel[kSize+j] = kernel[kSize-j] = normpdf(float(j), sigma);
+		}
+
+		//get the normalization factor (as the gaussian has been clamped)
+		for (int j = 0; j < mSize; ++j)
+		{
+			Z += kernel[j];
+		}
+
+		//read out the texels
+		for (int i=-kSize; i <= kSize; ++i)
+		{
+			for (int j=-kSize; j <= kSize; ++j)
+			{
+				final_colour += kernel[kSize+j]*kernel[kSize+i]*texture2D(tex, uv + offset *  vec2(float(i),float(j))).rgb;
+
+			}
+		}
+    return final_colour/(Z*Z);
+}
+
+void main() {
+  vec2 uv = gl_FragCoord.xy / u_resolution.xy;
+    vec2 offest = 1. / u_resolution.xy;
+    vec3 color = guassionBlur(u_texture_0, uv, offest);
     gl_FragColor = vec4(color, 1.);
 }
---
+++ test/surface-blur.glsl
@@ -0,0 +1,42 @@
+#ifdef GL_ES
+precision highp float;
+#endif
+
+uniform vec2 u_resolution;
+uniform vec2 u_mouse;
+uniform float u_time;
+uniform sampler2D u_texture_0;
+
+vec3 surfaceBlur(in sampler2D tex, in vec2 uv, in vec2 offset){
+    const float Y = 68.0;
+    const float r = 5.;
+    // rgb Separate calculation
+    vec3 center_c = texture2D(tex, uv).rgb;
+    vec3 sum_c = vec3(0.0);
+    for(int i = 0; i < 3; i++){
+    	float sum_i = 0.0;
+        float norm = 0.0;
+        for(float x = -r; x <= r; x++){
+
+           	for(float y = -r; y <= r; y++){
+               	vec2 uv2 = uv + vec2(x * offset.x, y * offset.y);
+               	vec3 cur_c = texture2D(tex, uv2).rgb;
+
+                float para = 1.0 - abs(center_c[i] - cur_c[i]) * 255.0 / (2.5 * Y);
+				sum_i += para * cur_c[i] * 255.0;
+                norm += para;
+           	}
+        }
+        sum_c[i] = sum_i / norm;
+    }
+
+	return sum_c / 255.0;
+}
+
+void main() {
+
+    vec2 uv = gl_FragCoord.xy/u_resolution.xy;
+    vec2 offest = 1. / u_resolution.xy;
+    vec3 color = surfaceBlur(u_texture_0,uv,offest);
+    gl_FragColor = vec4(color, 1.);
+}
